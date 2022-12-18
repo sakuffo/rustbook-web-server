0f7ee493ad73436010eddeff34b1ab8ea593845c
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-17 22:03:51 -0500

    response strated

--- src/main.rs
+++ src/main.rs
@@ -21,5 +21,6 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\n";
+    stream.write(response.as_bytes()).unwrap();
 }
