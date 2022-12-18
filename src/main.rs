b77498079103ae614a26c313bcb472d7a4aa1c70
Stephen Akuffo <sa.akuffo@gmail.com>
2022-12-18 12:23:57 -0500

    Single Threaded Server working and serving html

--- src/main.rs
+++ src/main.rs
@@ -30,19 +30,3 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
 }
-
-// if request_line == "GET / HTTP/1.1" {
-//     let status_line = "HTTP/1.1 200 OK";
-//     let contents = fs::read_to_string("hello.html").unwrap();
-//     let length = contents.len();
-
-//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-//     stream.write(response.as_bytes()).unwrap();
-// } else {
-//     let status_line = "HTTP/1.1 404 NOT FOUND";
-//     let contents = fs::read_to_string("404.html").unwrap();
-//     let length = contents.len();
-
-//     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-//     stream.write(response.as_bytes()).unwrap();
-// }
